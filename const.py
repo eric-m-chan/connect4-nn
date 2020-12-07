018e2e6d45e86cbb2c4c8f4e27ee5b915f2b7d3a
Eric Chan <echan044@ucr.edu>
2020-12-06 18:47:07 -0800

    static variables

---
+++ const.py
@@ -0,0 +1,15 @@
+#values that will be printed
+PLAYER_1_PIECE  = '1'
+PLAYER_2_PIECE  = '2'
+#encoding of board values
+EMPTY_VAL       = 100
+PLAYER_1_VAL    = 1
+PLAYER_2_VAL    = -1
+
+GAME_STATE_DRAW = 0
+GAME_NOT_OVER   = 2
+
+#standard connect 4 board size
+NUM_ROW = 6
+NUM_COL = 7
+EMPTY = ' '
